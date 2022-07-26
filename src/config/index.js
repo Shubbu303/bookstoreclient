c13c1db5df8420e1172fbc376f833b049d49c961
Shubbu303 <109985384+Shubbu303@users.noreply.github.com>
2022-07-26 22:40:39 +0530

    backend config chnages

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const environmentUrls = new Map();
 environmentUrls.set("localhost", "http://localhost:8080");
 environmentUrls.set(
   "bookstoreclient-0.herokuapp.com",
-  "https://bookstore-0.herokuapp.com"
+  "https://book-store-001.herokuapp.com"
 );
 
 export default environmentUrls.get(window.location.hostname);
